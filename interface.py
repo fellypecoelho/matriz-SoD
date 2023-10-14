c3d92b9e7a741801c87dd6f49ed00bd23bf1f5fc
Fellype Coelho <71403359+fellypecoelho@users.noreply.github.com>
2023-10-14 19:02:40 -0300

    Update interface.py
    Atualização de Código.
    Refiz a interface usando orientação objeto, e criei uma função melhor para o menu mostrar os frames com mais eficiência

--- interface.py
+++ interface.py
@@ -1,134 +1,94 @@
-from tkinter import *
-from tkinter import ttk
 from customtkinter import *
 
+class App(CTk):
+    def __init__(self):
+        super().__init__()
 
+        # Configurações iniciais e inicialização de variáveis
+        self.layout_config()  # Configurações da janela
+        self.frames = {}  # Dicionário para armazenar os frames
+        self.menuLateral()  # Cria o menu lateral
+        self.current_frame = None  # Inicializa o frame atual como vazio
 
-def show_home_frame():
-    perfilFrame.grid_forget()
-    sistemaFrame.grid_forget()
-    usuarioFrame.grid_forget()
-    homeFrame.grid(row=0, column=1, padx=(5, 0), pady=(0,0), sticky='nswe', columnspan=1)
+        # Primeiro, crie todos os frames, incluindo o 'frameHome'.
+        self.home()
+        self.perfil()
+        self.sistema()
+        self.usuario()
 
-    btn_homeMenu.configure(fg_color='#1f6aa5')
-    btn_perfilMenu.configure(fg_color='#2b2b2b')
-    btn_sistemaMenu.configure(fg_color='#2b2b2b')
-    btn_usuarioMenu.configure(fg_color='#2b2b2b')
+        # Em seguida, exiba o frame 'frameHome' como padrão.
+        self.show_home_frame()
 
+    def layout_config(self):
+        # Configurações iniciais da janela
+        self.title('Escola XYZ')  # Define o título da janela
+        self.geometry('910x550')  # Define o tamanho da janela
+        self.resizable(width=True, height=True)  # Permite redimensionar a janela
 
-def show_perfil_frame():
-    homeFrame.grid_forget()
-    sistemaFrame.grid_forget()
-    usuarioFrame.grid_forget()
-    perfilFrame.grid(row=0, column=1, padx=(5, 0), pady=(0,0), sticky='nswe', columnspan=1)
+    def menuLateral(self):
+        frameMenu = CTkFrame(self, width=150, height=500, corner_radius=0)
+        frameMenu.place(x=0, y=0)
 
-    btn_homeMenu.configure(fg_color='#2b2b2b')
-    btn_perfilMenu.configure(fg_color='#1f6aa5')
-    btn_sistemaMenu.configure(fg_color='#2b2b2b')
-    btn_usuarioMenu.configure(fg_color='#2b2b2b')
+        # Botões do menu lateral
+        btn_menuHome = CTkButton(frameMenu, text='Home', font=('Arial', 15, 'bold'), text_color='#c3c3c3',
+                              fg_color='#1f6aa5', corner_radius=0, width=150, height=50, anchor='w', border_spacing=10,
+                              command=self.show_home_frame)
+        btn_menuHome.place(x=0, y=0)
+        btn_menuPerfil = CTkButton(frameMenu, text='Perfil', font=('Arial', 15, 'bold'), text_color='#c3c3c3',
+                                fg_color='#1f6aa5', corner_radius=0, width=150, height=50, anchor='w',
+                                border_spacing=10, command=self.show_perfil_frame)
+        btn_menuPerfil.place(x=0, y=50)
+        btn_menuSistema = CTkButton(frameMenu, text='Sistema', font=('Arial', 15, 'bold'), text_color='#c3c3c3',
+                                 fg_color='#1f6aa5', corner_radius=0, width=150, height=50, anchor='w',
+                                 border_spacing=10, command=self.show_sistema_frame)
+        btn_menuSistema.place(x=0, y=100)
+        btn_menuUsuario = CTkButton(frameMenu, text='Usuário', font=('Arial', 15, 'bold'), text_color='#c3c3c3',
+                                 fg_color='#1f6aa5', corner_radius=0, width=150, height=50, anchor='w',
+                                 border_spacing=10, command=self.show_usuario_frame)
+        btn_menuUsuario.place(x=0, y=150)
 
-def show_sistema_frame():
-    homeFrame.grid_forget()
-    perfilFrame.grid_forget()
-    usuarioFrame.grid_forget()
-    sistemaFrame.grid(row=0, column=1, padx=(5, 0), pady=(0,0), sticky='nswe', columnspan=1)
+    def show_frame(self, frame):
+        if self.current_frame:
+            self.current_frame.place_forget()  # Oculta o frame atual
+        frame.place(x=160, y=0)  # Exibe o novo frame
+        self.current_frame = frame  # Atualiza o frame atual
 
-    btn_homeMenu.configure(fg_color='#2b2b2b')
-    btn_perfilMenu.configure(fg_color='#2b2b2b')
-    btn_sistemaMenu.configure(fg_color='#1f6aa5')
-    btn_usuarioMenu.configure(fg_color='#2b2b2b')
+    def show_home_frame(self):
+        self.show_frame(self.frames['home'])  # Exibe o frame 'home' e atualiza o frame atual
 
-def show_usuario_frame():
-    homeFrame.grid_forget()
-    perfilFrame.grid_forget()
-    sistemaFrame.grid_forget()
-    usuarioFrame.grid(row=0, column=1, padx=(5, 0), pady=(0,0), sticky='nswe', columnspan=1)
+    def show_perfil_frame(self):
+        self.show_frame(self.frames['perfil'])  # Exibe o frame 'perfil' e atualiza o frame atual
 
-    btn_homeMenu.configure(fg_color='#2b2b2b')
-    btn_perfilMenu.configure(fg_color='#2b2b2b')
-    btn_sistemaMenu.configure(fg_color='#2b2b2b')
-    btn_usuarioMenu.configure(fg_color='#1f6aa5')
+    def show_sistema_frame(self):
+        self.show_frame(self.frames['sistema'])  # Exibe o frame 'sistema' e atualiza o frame atual
 
+    def show_usuario_frame(self):
+        self.show_frame(self.frames['usuario'])  # Exibe o frame 'usuario' e atualiza o frame atual
 
+    def home(self):
+        frameHome = CTkFrame(self, fg_color='transparent', width=750, height=500, corner_radius=0)
+        self.frames['home'] = frameHome  # Armazena o frame 'home' no dicionário
+        labelTitulo_frame = CTkLabel(frameHome, text='H O M E', font=('Arial', 20))
+        labelTitulo_frame.place(x=10, y=10)
 
+    def perfil(self):
+        framePerfil = CTkFrame(self, fg_color='transparent', width=750, height=500, corner_radius=0)
+        self.frames['perfil'] = framePerfil  # Armazena o frame 'perfil' no dicionário
+        labelTitulo_frame = CTkLabel(framePerfil, text='P E R F I L', font=('Arial', 20))
+        labelTitulo_frame.place(x=10, y=10)
 
+    def sistema(self):
+        frameSistema = CTkFrame(self, fg_color='transparent', width=750, height=500, corner_radius=0)
+        self.frames['sistema'] = frameSistema  # Armazena o frame 'sistema' no dicionário
+        labelTitulo_frame = CTkLabel(frameSistema, text='S I S T E M A', font=('Arial', 20))
+        labelTitulo_frame.place(x=10, y=10)
 
-app = CTk()
-#Tema do Sistema
-app._set_appearance_mode('dark') #Modos: "system", "dark" e "light"
-set_default_color_theme("blue") #Temas: "blue" (standard), "green", "dark-blue"
+    def usuario(self):
+        frameUsuario = CTkFrame(self, fg_color='transparent', width=750, height=500, corner_radius=0)
+        self.frames['usuario'] = frameUsuario  # Armazena o frame 'usuario' no dicionário
+        labelTitulo_frame = CTkLabel(frameUsuario, text='U S U Á R I O', font=('Arial', 20))
+        labelTitulo_frame.place(x=10, y=10)
 
-app.title('Cadastro Sistema')
-
-#Dimenções da Janela
-app.geometry('900x500')
-app.resizable(width=False, height=False) #Bloquear dimensões
-
-
-
-#LAYOUT
-menuFrame=CTkFrame(app, fg_color='#2b2b2b')
-menuFrame.grid(row=0, column=0, padx=(0, 0), pady=(0,0), sticky='nswe', columnspan=1)
-menuFrame.configure(width=250, height=500)
-
-homeFrame=CTkFrame(app, fg_color='#242424')
-homeFrame.grid(row=0, column=1, padx=(5, 0), pady=(0,0), sticky='nswe', columnspan=1)
-homeFrame.configure(width=650, height=500)
-
-perfilFrame=CTkFrame(app, fg_color='#242424')
-perfilFrame.grid(row=0, column=1, padx=(5, 0), pady=(0,0), sticky='nswe', columnspan=1)
-perfilFrame.configure(width=650, height=500)
-perfilFrame.grid_forget()  # Oculta o frame perfilFrame
-
-sistemaFrame=CTkFrame(app, fg_color='#242424')
-sistemaFrame.grid(row=0, column=1, padx=(5, 0), pady=(0,0), sticky='nswe', columnspan=1)
-sistemaFrame.configure(width=650, height=500)
-sistemaFrame.grid_forget()  # Oculta o frame perfilFrame
-
-usuarioFrame=CTkFrame(app, fg_color='#242424')
-usuarioFrame.grid(row=0, column=1, padx=(5, 0), pady=(0,0), sticky='nswe', columnspan=1)
-usuarioFrame.configure(width=650, height=500)
-usuarioFrame.grid_forget()  # Oculta o frame perfilFrame
-
-
-
-
-
-#CONTEÚDO MENUFRAME
-btn_homeMenu = CTkButton(menuFrame, text='Home', font=('Arial', 15, 'bold'), text_color='#c3c3c3', fg_color='#1f6aa5', corner_radius=0, height=40, anchor='w', border_spacing=15, command=show_home_frame)
-btn_homeMenu.grid(row=0, column=0, padx=0, pady=(0,0), sticky='nswe', columnspan=1)
-
-btn_perfilMenu = CTkButton(menuFrame, text='Perfil', font=('Arial', 15, 'bold'), text_color='#c3c3c3', fg_color='#2b2b2b', corner_radius=0, height=40, anchor='w', border_spacing=15, command=show_perfil_frame)
-btn_perfilMenu.grid(row=1, column=0, padx=0, pady=(2,0), sticky='nswe', columnspan=1)
-
-btn_sistemaMenu = CTkButton(menuFrame, text='Sistema', font=('Arial', 15, 'bold'), text_color='#c3c3c3', fg_color='#2b2b2b', corner_radius=0, height=40, anchor='w', border_spacing=15, command=show_sistema_frame)
-btn_sistemaMenu.grid(row=2, column=0, padx=0, pady=(2,0), sticky='nswe', columnspan=1)
-
-btn_usuarioMenu = CTkButton(menuFrame, text='Usuário', font=('Arial', 15, 'bold'), text_color='#c3c3c3', fg_color='#2b2b2b', corner_radius=0, height=40, anchor='w', border_spacing=15, command=show_usuario_frame)
-btn_usuarioMenu.grid(row=3, column=0, padx=0, pady=(2,0), sticky='nswe', columnspan=1)
-
-
-
-
-#CONTEÚDO MAINFRAME
-label_tituloHome = CTkLabel(homeFrame, width=200, text='H O M E', font=('Arial', 20))
-label_tituloHome.grid(row=0, column=0, padx=10, pady=(30,10), sticky='nswe', columnspan=2)
-
-
-
-label_tituloPerfil = CTkLabel(perfilFrame, width=200, text='P E R F I L', font=('Arial', 20))
-label_tituloPerfil.grid(row=0, column=0, padx=10, pady=(30,10), sticky='nswe', columnspan=2)
-
-
-label_tituloSistema = CTkLabel(sistemaFrame, width=200, text='S I S T E M A', font=('Arial', 20))
-label_tituloSistema.grid(row=0, column=0, padx=10, pady=(30,10), sticky='nswe', columnspan=2)
-
-
-label_tituloUsuario = CTkLabel(usuarioFrame, width=200, text='U S U Á R I O', font=('Arial', 20))
-label_tituloUsuario.grid(row=0, column=0, padx=10, pady=(30,10), sticky='nswe', columnspan=2)
-
-
-
-
-app.mainloop()
-
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
